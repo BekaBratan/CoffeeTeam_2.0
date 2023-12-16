b13118951a90e4910ed90b18d8708aaf87100e72
lemon4iiik <musafar2104@mail.ru>
2023-12-16 15:10:17 +0600

    buttons and listView
    make a listView

--- app/src/main/java/com/example/coffeeproject/MainActivity.kt
+++ app/src/main/java/com/example/coffeeproject/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.coffeeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        val userData: EditText = findViewById(R.id.user_data)
+        val button: Button = findViewById(R.id.button)
+
+        val todos: MutableList<String> = mutableListOf()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
+        listView.adapter = adapter
+
+        button.setOnClickListener{
+            val text = userData.text.toString().trim()
+            if (text != "")
+                adapter.insert(text, 0)
+        }
+
+        listView.setOnItemClickListener{ adapterView, view, i, l ->
+            val text = listView.getItemAtPosition(i).toString()
+            adapter.remove(text)
+
+            Toast.makeText(this, "deleted: $text", Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
