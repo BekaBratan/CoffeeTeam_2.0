b583bfcf1548eeeeff51eb0545a923df1ea35ab4
lemon4iiik <musafar2104@mail.ru>
2023-12-16 16:02:16 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/coffeeproject/MainActivity.kt
+++ app/src/main/java/com/example/coffeeproject/MainActivity.kt
@@ -13,28 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val listView = findViewById<ListView>(R.id.list_view)
-        val userData: EditText = findViewById(R.id.user_data)
-        val button: Button = findViewById(R.id.button)
-
-        val todos: MutableList<String> = mutableListOf()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
-        listView.adapter = adapter
-
-        button.setOnClickListener{
-            val text = userData.text.toString().trim()
-            if (text != "")
-                adapter.insert(text, 0)
-        }
-
-        listView.setOnItemClickListener{ adapterView, view, i, l ->
-            val text = listView.getItemAtPosition(i).toString()
-            adapter.remove(text)
-
-            Toast.makeText(this, "deleted: $text", Toast.LENGTH_LONG).show()
-        }
-
-
     }
 }
